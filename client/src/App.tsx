32d0974cd8a3eb3f310817c8cb903695cf54ca1a
Evan Conway <evanconwaymusic@gmail.com>
2024-04-10 14:27:28 -0400

    fix spelling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,11 @@ function App() {
         The Monty Hall problem comes from a game show. The contestent is shown 3
         doors. Behind one is a prize, but behind the other 2 are dud prizes.
         First the contestant must choose a door they believe the prize is
-        behind. Then of the remaining 2 doors, the host will reveal a dud from
-        one of them. The host knows which door the prize is behind and will
-        ALWAYS reveal a dud door. After this the contestant is allowed to choose
-        to stay with the door they originally picked, or switch the other close
-        door. Is there an optimal choice, and which is it?
+        behind. Then of the remaining 2 doors, the host will open one to reveal
+        a dud. The host knows which door the prize is behind and will ALWAYS
+        reveal a dud door. After this the contestant is allowed to choose to
+        stay with their original door, or switch to the other closed door. Is
+        there an optimal choice, and which is it?
       </p>
       <h1>The Solution</h1>
       <p>
