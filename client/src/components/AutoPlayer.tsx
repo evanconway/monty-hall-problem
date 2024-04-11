fa4993d3c4fb67647fe4995056ee04bb7cf1dc6f
Evan Conway <43253820+evanconway@users.noreply.github.com>
2024-04-11 01:49:57 -0400

    add functioning auto player

--- client/src/components/AutoPlayer.tsx
+++ client/src/components/AutoPlayer.tsx
@@ -10,6 +10,22 @@ const AutoPlayer = () => {
   const [games, setGames] = useState<MontyHall[]>([]); // boolean indicates game won
   const [winLoss, setWinLoss] = useState({ win: 0, lose: 0 });
   const [alwaysSwitch, setAlwaysSwitch] = useState(true);
+  const [autoTimeoutId, setAutoTimeoutId] = useState<number | null>(null);
+
+  const [playing, setPlaying] = useState(false);
+  useEffect(() => {
+    if (playing === false) return;
+    if (games.length >= gameCount) {
+      setPlaying(false);
+      setAutoTimeoutId(null);
+      return;
+    }
+    setAutoTimeoutId(
+      setTimeout(() => {
+        setGames([...games, montyHallGetPlayedGame(alwaysSwitch)]);
+      }),
+    );
+  }, [playing, games, alwaysSwitch, setAutoTimeoutId]);
 
   useEffect(() => {
     setWinLoss(
@@ -26,30 +42,34 @@ const AutoPlayer = () => {
 
   return (
     <div>
-      <button onClick={() => setAlwaysSwitch(!alwaysSwitch)}>
+      <button
+        disabled={autoTimeoutId !== null}
+        onClick={() => setAlwaysSwitch(!alwaysSwitch)}
+      >
         Toggle Always Switch
       </button>
       <div>{`Always Switch: ${alwaysSwitch ? "On" : "Off"}`}</div>
       <button
+        disabled={autoTimeoutId !== null}
         onClick={() => {
-          setGames(
-            [...new Array(gameCount)].map(() =>
-              montyHallGetPlayedGame(alwaysSwitch),
-            ),
-          );
+          if (autoTimeoutId !== null) clearTimeout(autoTimeoutId);
+          setGames([]);
+          setPlaying(true);
         }}
       >
         {`Play ${gameCount} Games`}
       </button>
       <div>{`wins: ${winLoss.win} losses: ${winLoss.lose}`}</div>
-      <ul>
-        {games?.map((game, i) => (
-          <li
-            style={{ textAlign: "start" }}
-            key={i}
-          >{`prize: ${game.prizeDoor}, chosen: ${game.contestantDoorSelected} switched: ${game.contestantSwitch} won: ${game.prizeDoor === montyHallGetContestantDoorChoice(game)}`}</li>
-        ))}
-      </ul>
+      <div style={{ height: "60vh", overflowY: "scroll" }}>
+        <ul>
+          {games?.map((game, i) => (
+            <li
+              style={{ textAlign: "start" }}
+              key={i}
+            >{`prize: ${game.prizeDoor}, chosen: ${game.contestantDoorSelected}, switched: ${game.contestantSwitch}, won: ${game.prizeDoor === montyHallGetContestantDoorChoice(game)}`}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
